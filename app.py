02dde5a7d6ed4ee0e36df9443c0553e78692f871
kirangajjana <kiran.gajjana@gmail.com>
2024-11-15 22:35:43 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,redirect,url_for
+from class1 import kiran
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def home():
+    k=kiran()
+    return k.other()
+@app.route("/home/<int:num>")
+def hello(num):
+    return f"<h1>you number is {num},  and the sum {num+10}</h1>"
+
+@app.route("/welcome/<name>")
+def welcomepage(name):
+    return f"hello mr {name} welcome you to this page"   #synamic url is working here
+
+@app.route("/pass/<sname>/<snum>")
+def passed(sname,snum):
+    return f"<h1>hey user {sname.title()} you are passed with  {snum} marks</h1>"
+
+
+
+
+@app.route("/examresults/<name>/<int:num>")
+def data(name,num):
+    if num < 30:
+        return redirect(url_for("passed",sname=name,snum=num))
+    else:
+        return "<h1>hello kiran gajjana hoe are you</h1>"
+
+if __name__=="__main__":
+    app.run(debug=True)
