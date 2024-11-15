94801065b8a8361adb865af2dcb857ab95f80758
kirangajjana <kiran.gajjana@gmail.com>
2024-11-16 05:06:31 +0530

    home.html

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask,redirect,url_for
+from flask import Flask,redirect,url_for,render_template
 
 
 app=Flask(__name__)
 
-
+@app.route("/home")
+def homepage():
+    return render_template("home.html",title="home")
 
 
 if __name__=="__main__":
