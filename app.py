74cd2bbde8b2c8d6a11223b6e6409de822210049
kirangajjana <kiran.gajjana@gmail.com>
2024-11-16 05:36:29 +0530

    added about.html

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app=Flask(__name__)
 @app.route("/home")
 def homepage():
     return render_template("home.html",title="home")
-
+@app.route("/about")
+def aboutpage():
+    return render_template("about.html",title="about")
 
 if __name__=="__main__":
     app.run(debug=True)
